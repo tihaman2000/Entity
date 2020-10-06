27144451dcbd9b6abd117221d03f06ff3e7fccbf
devhaidang <tihaman2000@gmail.com>
2020-10-06 08:45:00 +0700

    no message

---
+++ src/main/java/com/poly/Entity/App.java
@@ -0,0 +1,13 @@
+package com.poly.Entity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
